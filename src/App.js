b442979e9946b78c426e3640d141a6adc5adc1e7
Chavdar Yotov <chavdar.yotov@arm.com>
2024-01-07 11:41:10 +0000

    Update ingredients

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+// My button
 function MyButton() {
   return (
     <button>
@@ -8,12 +9,25 @@ function MyButton() {
   );
 }
 
-const theList = [
+// A list and its representation
+const sourdoughIngredients = [
   { title: 'Water', id: 1},
   { title: 'Flour', id: 2},
-  { title: 'Starter', id: 3}
-  { title: 'Salt', id: 4},
+  { title: 'Starter', id: 3},
+  { title: 'Salt', id: 4}
 ];
+
+function Sourdough() {
+  const listItems = sourdoughIngredients.map(ingredient =>
+    <li key={ingredient.id}>
+      {ingredient.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+};
 
 export default function App() {
   return (
@@ -21,6 +35,10 @@ export default function App() {
       <h1> Hi and welcome to my resume :-) </h1>
       <h2> Here's a button: </h2>
       <MyButton />
+
+      <h2>Here are the ingredients for sourdough:</h2>
+
     </div>
   );
 }
+
